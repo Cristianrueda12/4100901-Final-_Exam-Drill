2ee1ab6ddf7f64f3ed8360609a49b2516f43a371
Cristianrueda12 <161840405+Cristianrueda12@users.noreply.github.com>
2024-09-09 18:42:28 -0500

    Librerias keypad
    Se integraron las librerias para el uso del teclado matricial

---
+++ Core/Inc/keypad.h
@@ -0,0 +1,15 @@
+/*
+ * keypad.h
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: Cristian
+ */
+
+#ifndef INC_KEYPAD_H_
+#define INC_KEYPAD_H_
+
+
+
+
+
+#endif /* INC_KEYPAD_H_ */
---
+++ Core/Src/keypad.c
@@ -0,0 +1,6 @@
+/*
+ * keypad.c
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: Cristian
+ */
